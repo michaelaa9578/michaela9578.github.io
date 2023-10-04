9f40c22ba1dcf683f767ba4f451768946b055c1a
michaelaa9578 <146866763+michaelaa9578@users.noreply.github.com>
2023-10-03 22:46:24 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Michaela A"; // update the part BETWEEN the "quotes"
+let userName = "michaelaa9578"; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
